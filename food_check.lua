0dd3ac3088c7c48a8a68efc643992b35d7b7fdc1
Apollo <beanzilla@beanzilla.net>
2022-02-28 15:41:10 -0500

    Changed eating effect
      Removed annoying graphical effect which is played by minetest engine.
    
      Closes: Issue #2

--- food_check.lua
+++ food_check.lua
@@ -19,7 +19,17 @@ end
 if medblocks.hunger_installed == true then
     minetest.register_craftitem("medblocks:feeding", {
         description = "Feeding",
-        inventory_image = "medblocks_redplus2.png"
+        inventory_image = "medblocks_empty.png",
+        -- minetest.do_item_eat causes a graphical "crumbs" effect,
+        -- I didn't want that so I'm using a empty/transucent image.
+        groups = {not_in_creative_inventory = 1}, -- Don't show this in the list of items (you really don't want this item)
+        on_use = minetest.item_eat(1)
+    })
+    -- Just in case some moron gave themselves this item, allow it to be garbage.
+    minetest.register_craft({
+        type = "fuel",
+        recipe = "medblocks:feeding",
+        burntime = 1
     })
 end
 

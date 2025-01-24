bf01263f597f6678f058190e96fd415a35ba2a8e
Beanzilla <david2bond2@gmail.com>
2025-01-24 11:57:47 -0500

    Update init.lua
    Fixed a Geneva Convention violation

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 medblocks = {}
 
 medblocks.MODPATH = minetest.get_modpath("medblocks")
-medblocks.VERSION = "1.2.2"
+medblocks.VERSION = "1.2.3"
 medblocks.settings = {
     mednode = {
         allow_crafting = false,
